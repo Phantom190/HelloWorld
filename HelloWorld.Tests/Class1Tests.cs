0c98bd6b2bfa4f2bd6fef23f4f6d0643122d1099
Phantom190 <18465494+Phantom190@users.noreply.github.com>
2018-09-18 08:55:15 +0100

    Add files

---
+++ HelloWorld.Tests/Class1Tests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Xunit;
+
+namespace HelloWorld.Tests
+{
+    public class Class1Tests
+    {
+        [Fact]
+        void TestMethod()
+        {
+            Assert.True(Class1.MyMethod());
+        }
+    }
+}
---
+++ HelloWorld/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    public class Class1
+    {
+        public static bool MyMethod()
+        {
+            return true;
+        }
+    }
+}
